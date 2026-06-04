9f6fb1425813a68487734129cd330d8a49baa177
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-04 12:19:39 +0300

    Create RefundManagementApplication.java

---
+++ project-08-refund-management-platform/RefundManagementApplication.java
@@ -0,0 +1,15 @@
+package com.refund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefundManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                RefundManagementApplication.class,
+                args
+        );
+    }
+}
