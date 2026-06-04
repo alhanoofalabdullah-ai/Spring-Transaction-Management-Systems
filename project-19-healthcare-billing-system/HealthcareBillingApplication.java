513eb43dcf40d1a129ee64e3de808b4fee335d2e
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-04 16:32:54 +0300

    Create HealthcareBillingApplication.java

---
+++ project-19-healthcare-billing-system/HealthcareBillingApplication.java
@@ -0,0 +1,15 @@
+package com.healthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareBillingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                HealthcareBillingApplication.class,
+                args
+        );
+    }
+}
