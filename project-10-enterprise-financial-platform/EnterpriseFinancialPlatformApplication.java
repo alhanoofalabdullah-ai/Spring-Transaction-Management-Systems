58c5208667ae5cf578c1e5ee73e5e5e85ca073fa
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-04 12:49:13 +0300

    Create EnterpriseFinancialPlatformApplication.java

---
+++ project-10-enterprise-financial-platform/EnterpriseFinancialPlatformApplication.java
@@ -0,0 +1,15 @@
+package com.financial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseFinancialPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                EnterpriseFinancialPlatformApplication.class,
+                args
+        );
+    }
+}
