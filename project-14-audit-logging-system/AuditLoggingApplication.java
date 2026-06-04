7a42c75fc2dea2393ec1e47e720e097122912209
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-04 14:30:55 +0300

    Create AuditLoggingApplication.java

---
+++ project-14-audit-logging-system/AuditLoggingApplication.java
@@ -0,0 +1,15 @@
+package com.audit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditLoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                AuditLoggingApplication.class,
+                args
+        );
+    }
+}
