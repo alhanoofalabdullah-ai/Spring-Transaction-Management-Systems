352b9a59e8527ca706f17f5bf08e382bbb2fe7ce
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-04 12:34:19 +0300

    Create SubscriptionBillingApplication.java

---
+++ project-09-subscription-billing-engine/SubscriptionBillingApplication.java
@@ -0,0 +1,15 @@
+package com.subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionBillingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                SubscriptionBillingApplication.class,
+                args
+        );
+    }
+}
