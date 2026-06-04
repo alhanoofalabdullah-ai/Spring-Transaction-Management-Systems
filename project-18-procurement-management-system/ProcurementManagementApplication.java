c0306a1f725c361860ba27dc85469027e430d4bf
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-04 16:17:55 +0300

    Create ProcurementManagementApplication.java

---
+++ project-18-procurement-management-system/ProcurementManagementApplication.java
@@ -0,0 +1,15 @@
+package com.procurement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcurementManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                ProcurementManagementApplication.class,
+                args
+        );
+    }
+}
