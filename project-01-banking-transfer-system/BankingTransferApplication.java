6f8f15eb47ac0db687a3767016fad666d3f226cc
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-04 08:32:32 +0300

    Create BankingTransferApplication.java

---
+++ project-01-banking-transfer-system/BankingTransferApplication.java
@@ -0,0 +1,15 @@
+package com.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingTransferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                BankingTransferApplication.class,
+                args
+        );
+    }
+}
