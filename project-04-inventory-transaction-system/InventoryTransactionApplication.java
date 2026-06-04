7ad7f40a6c8cbce7bc41b5a4e246eacae656b690
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-04 11:29:24 +0300

    Create InventoryTransactionApplication.java

---
+++ project-04-inventory-transaction-system/InventoryTransactionApplication.java
@@ -0,0 +1,15 @@
+package com.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                InventoryTransactionApplication.class,
+                args
+        );
+    }
+}
