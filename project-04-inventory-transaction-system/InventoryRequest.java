9c370c475ce125397e50b8fdb7b9c75e33c0af1e
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-04 11:27:15 +0300

    Create InventoryRequest.java

---
+++ project-04-inventory-transaction-system/InventoryRequest.java
@@ -0,0 +1,24 @@
+package com.inventory;
+
+public class InventoryRequest {
+
+    private Long productId;
+
+    private Integer quantity;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
