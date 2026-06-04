cfb623678470d7d6758d72bb316abd654b472a9d
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-04 17:02:20 +0300

    Create EnterpriseTransactionApplication.java

---
+++ project-20-enterprise-transaction-platform/EnterpriseTransactionApplication.java
@@ -0,0 +1,15 @@
+package com.enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                EnterpriseTransactionApplication.class,
+                args
+        );
+    }
+}
